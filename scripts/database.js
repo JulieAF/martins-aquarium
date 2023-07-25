afa44d8f54f593dea40240090b83e4647913b3cb
Julie Foster <juliefoster6695@gmail.com>
2023-07-25 14:33:53 -0400

    put html in main.js and fishlist.js

--- scripts/database.js
+++ scripts/database.js
@@ -1,15 +1,17 @@
 const database = {
   fish: [
     {
-      image: "",
+      image:
+        "https://friendlystock.com/wp-content/uploads/2020/03/9-cute-brown-fish-cartoon-clipart.jpg",
       name: "Bart",
-      food: "crustaceans",
+      food: "Crustaceans",
       length: 3,
       species: "Spiny Fish",
       location: "Middle of Ocean",
     },
     {
-      image: "",
+      image:
+        "https://www.caricatures-ireland.com/blog/wp-content/uploads/2018/10/fish-cartoon-clipart-free-700px.jpg",
       name: "Bertha",
       food: "Gnats",
       length: 5,
@@ -17,7 +19,8 @@ const database = {
       location: "The Crick",
     },
     {
-      image: "",
+      image:
+        "https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvam9iNjc4LTIzMi14LmpwZw.jpg",
       name: "Bob",
       food: "Molecules",
       length: 3,
@@ -25,7 +28,8 @@ const database = {
       location: "Swimming Pool",
     },
     {
-      image: "",
+      image:
+        "https://previews.123rf.com/images/julos/julos1607/julos160761431/82496317-cartoon-fish-with-big-lips.jpg",
       name: "Petunia",
       food: "Jello",
       length: 16,
@@ -33,7 +37,8 @@ const database = {
       location: "Frozen Lake",
     },
     {
-      image: "",
+      image:
+        "https://images.freeimages.com/images/premium/previews/4446/44467662-cartoon-funny-fish.jpg",
       name: "Ralf",
       food: "Paper",
       length: 6,
@@ -41,8 +46,9 @@ const database = {
       location: "Coy Pond",
     },
     {
-      image: "",
-      name: "Bart",
+      image:
+        "https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvam9iNjg0LTA3Ny14LmpwZw.jpg",
+      name: "Grof",
       food: "Anxiety",
       length: 4,
       species: "Shocked Fish",
